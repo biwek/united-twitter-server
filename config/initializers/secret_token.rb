a392fa4eb183368d8242243e0b919231d4d24ded
Biwek Shrestha <biwekshrestha@biweks-mbp.default>
2014-04-11 12:17:44 +0100

    added twitter and figaro gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UnitedTwitterServer::Application.config.secret_key_base = 'eb1745178fa31f31f35ef61a9f3f2e71faf237ba122695836f398dc95688a000f361431f7dd28b898d0979edf8c808cbb5662b19a7b3402a4b0f98f5663516b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UnitedTwitterServer::Application.config.session_store :cookie_store, key: '_united-twitter-server_session'
